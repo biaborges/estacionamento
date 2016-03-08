e7e60bd294b7bb793cbc5b8794880895809bfbee
Beatriz Borges <beatrizoliveira.borges@bol.com.br>
2016-03-07 22:03:25 -0300

    Inclusão de estilo na pagina inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Pagina incial do projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css" />
 </head>
 <body>
 	<h1>Pagina inicial do projeto</h1>
+	<?php print "Impresso em PHP";?>
 </body>
 </html>
