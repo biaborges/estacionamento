5ca251b66985d7a431e38f8c5221c5d1843b53aa
Beatriz Borges <beatrizoliveira.borges@bol.com.br>
2016-03-07 21:54:41 -0300

    Estruturação da pagina inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "Pagina Incial do projeto";
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina incial do projeto</title>
+</head>
+<body>
+	<h1>Pagina inicial do projeto</h1>
+</body>
+</html>
